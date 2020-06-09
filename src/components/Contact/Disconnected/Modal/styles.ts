3c918fffb22da0cf86ade1bb7bca46ac9b382bd5
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-06-09 12:01:47 -0300

    Changes on Contact form

--- src/components/Contact/Disconnected/Modal/styles.ts
+++ src/components/Contact/Disconnected/Modal/styles.ts
@@ -3,7 +3,27 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
+  flex-direction: column;
 
   padding: 20px 40px;
+`;
+
+export const Title = styled.h3`
+  color: ${({ theme }) => theme.font.color.primary};
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 12px;
+`;
+
+export const BoxPhone = styled.div`
+  display: flex;
+  > div {
+    justify-content: flex-end;
+    width: 121px;
+    margin-right: 20px;
+    & + div {
+      margin-right: 0;
+      width: 100%;
+    }
+  }
 `;
