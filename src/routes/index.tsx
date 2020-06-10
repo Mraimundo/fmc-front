3fd5b96be07a89f13435b75b215db15683bc96f3
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-06-10 11:22:17 -0300

    Create route for first access

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,18 @@ import { Switch } from 'react-router-dom';
 
 import SignIn from 'pages/SignIn';
 import Dashboard from 'pages/Dashboard';
+import FirstAccess from 'pages/FirstAccess';
 import Route from './Route';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={SignIn} isPrivate={false} />
+    <Route
+      exact
+      path="/firstAccess"
+      component={FirstAccess}
+      isPrivate={false}
+    />
     <Route exact path="/dashboard" component={Dashboard} />
   </Switch>
 );
