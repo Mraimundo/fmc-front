40808d95466770c1501719e7799a636fae2cdfc7
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-06-16 19:07:24 -0300

    Accordion

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 
-import Accordion from 'components/shared/Accordion';
-import { Container, Content } from './styles';
+import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
       <h1>Dashboard</h1>
-      <Content>
-        <Accordion title="Teste">
-          <h1>Oi</h1>
-          <h2>Bye</h2>
-        </Accordion>
-      </Content>
     </Container>
   );
 };
