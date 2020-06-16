7b8008ef5ec3adc52474fedafe6f2b2cca808342
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-06-16 19:53:25 -0300

    Exporting Accordion

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -4,5 +4,6 @@ import PasswordInput from './PasswordInput';
 import Tooltip from './Tooltip';
 import TextArea from './TextArea';
 import Avatar from './Avatar';
+import Accordion from './Accordion';
 
-export { Button, Input, PasswordInput, Tooltip, TextArea, Avatar };
+export { Button, Input, PasswordInput, Tooltip, TextArea, Avatar, Accordion };
