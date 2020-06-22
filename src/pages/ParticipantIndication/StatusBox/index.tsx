eb8ac237c4a589d47288b208ddde48e4736ac39f
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-06-22 16:41:55 -0300

    Start page Partipant Indication

--- src/pages/ParticipantIndication/StatusBox/index.tsx
+++ src/pages/ParticipantIndication/StatusBox/index.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Icon from '@material-ui/icons/CropPortrait';
+import iconEmpty from 'assets/images/indication/status-empty.svg';
+import iconFilled from 'assets/images/indication/status-filled.svg';
+import { Rating } from '@material-ui/lab';
 
 import { Container, StatusIndicator, AddButton } from './styles';
 
@@ -6,7 +10,12 @@ const StatusBox: React.FC = () => {
   return (
     <Container>
       <h3>Equipe ativa no sistema</h3>
-      <StatusIndicator />
+      <Rating
+        name="customized-10"
+        defaultValue={2}
+        max={10}
+        icon={<img src={iconFilled} alt="teste" />}
+      />
       <AddButton />
     </Container>
   );
