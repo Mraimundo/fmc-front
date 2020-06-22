27c5179fe423c19201efd639f1ac4dac22f8c460
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-06-22 15:24:57 -0300

    Layout

--- src/pages/_layouts/index.tsx
+++ src/pages/_layouts/index.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ModalRegulations from 'components/Regulation/ModalAllRegulations';
 import { useAuth } from 'context/AuthContext';
 
+import { Container } from './styles';
+
 const Dashboard: React.FC = ({ children }) => {
   const { shouldShowRegulationsModal } = useAuth();
 
   return (
-    <>
+    <Container>
       {children}
       <ModalRegulations isOpen={shouldShowRegulationsModal} />
-    </>
+    </Container>
   );
 };
 
