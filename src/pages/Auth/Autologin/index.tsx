1f9d6f33eaa73db78a1bc633cf61b5b5ad8bca09
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-07-05 20:25:35 -0300

    Autologin

--- src/pages/Auth/Autologin/index.tsx
+++ src/pages/Auth/Autologin/index.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import history from 'services/history';
 
 const Autologin: React.FC = () => {
-  return <h1>Autologin</h1>;
+  const { search } = useLocation();
+
+  useEffect(() => {
+    const token = new URLSearchParams(search).get('token');
+    if (!token) {
+      history.push('/');
+    }
+  }, [search]);
+
+  return <h1>Autologin </h1>;
 };
 
 export default Autologin;
