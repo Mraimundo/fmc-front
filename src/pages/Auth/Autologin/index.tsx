69d42b0e72df57c633faf798a259a4d121e5ba72
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-07-05 19:58:58 -0300

    Autologin

---
+++ src/pages/Auth/Autologin/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Autologin: React.FC = () => {
+  return <h1>Autologin</h1>;
+};
+
+export default Autologin;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 
+import Autologin from 'pages/Auth/Autologin';
 import SignIn from 'pages/SignIn';
 import Dashboard from 'pages/Dashboard';
 import FirstAccess from 'pages/FirstAccess';
@@ -13,6 +14,7 @@ import Route from './Route';
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={SignIn} isPrivate={false} />
+    <Route exact path="/autologin" component={Autologin} isPrivate={false} />
     <Route exact path="/recover" component={SignIn} isPrivate={false} />
     <Route
       exact
