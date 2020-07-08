74ff4e6f70322c43f65c5acc262ccd4e589981cc
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-07-08 16:05:13 -0300

    Autologin

--- src/pages/Auth/Autologin/index.tsx
+++ src/pages/Auth/Autologin/index.tsx
@@ -1,20 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import history from 'services/history';
+import { useAuth } from 'context/AuthContext';
+import { useToast } from 'context/ToastContext';
 
 const Autologin: React.FC = () => {
   const { search } = useLocation();
-  const [token, setToken] = useState('');
+  const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   useEffect(() => {
-    const tokenParam = new URLSearchParams(search).get('token');
-    if (!tokenParam) {
+    const token = new URLSearchParams(search).get('token');
+    if (!token) {
       history.push('/');
+      return;
     }
-    setToken(tokenParam || '');
-  }, [search]);
+    const login = async () => {
+      try {
+        await signIn(token);
+      } catch {
+        addToast({
+          title: 'Token inválido',
+          type: 'error',
+        });
+        history.push('/');
+      }
+    };
+    login();
+  }, [search, signIn, addToast]);
 
-  return <h1>Autologin {token}</h1>;
+  return <h1>Aguarde enquanto o login automático é realizado ...</h1>;
 };
 
 export default Autologin;
