f122962cefedf1c98c3c65df3d0642100e0a8f84
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-07-16 11:44:13 -0300

    Show case Layout 1

--- src/pages/ShowCase/index.tsx
+++ src/pages/ShowCase/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useAuth } from 'context/AuthContext';
 import Layout1 from './Layout1';
 import Layout2 from './Layout2';
 
 const ShowCase: React.FC = () => {
-  return <Layout2 />;
+  const { participant } = useAuth();
+
+  return participant.profile === 'FMC' ? <Layout2 /> : <Layout1 />;
 };
 
 export default ShowCase;
