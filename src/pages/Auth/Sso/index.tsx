2aa78d4f3e64e4eed82447fb66d7e823ce341ee7
Fernando Almeida <lfalmeida@me.com>
2020-07-22 09:00:12 -0300

    adicionando exibiçao de mensagem sso

--- src/pages/Auth/Sso/index.tsx
+++ src/pages/Auth/Sso/index.tsx
@@ -2,13 +2,29 @@ import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import history from 'services/history';
 import { useAuth } from 'context/AuthContext';
+import { useToast } from 'context/ToastContext';
 
 const Sso: React.FC = () => {
   const { search } = useLocation();
   const { setToken } = useAuth();
+  const { addToast } = useToast();
 
   useEffect(() => {
     const token = new URLSearchParams(search).get('token');
+    let encodedMessage = new URLSearchParams(search).get('m');
+    let decodedMessage = '';
+
+    try {
+      if (encodedMessage) {
+        decodedMessage = decodeURIComponent(atob(encodedMessage));
+        addToast({
+          title: decodedMessage,
+          type: 'error',
+        });
+      }
+    } catch (e) {
+      encodedMessage = null;
+    }
 
     if (!token) {
       history.push('/');
@@ -19,7 +35,7 @@ const Sso: React.FC = () => {
       history.push('/');
     };
     login();
-  }, [search, setToken]);
+  }, [search, addToast, setToken]);
   return <h1>aguarde...</h1>;
 };
 
