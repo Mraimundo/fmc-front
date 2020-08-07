859e0f560f1cdd11640ff70ead0fc1701864e9fa
Gabriel Ferreira <gabriel.ferreira@iclinic.com.br>
2020-08-06 23:29:44 -0300

    test(home/header): add tests

--- src/components/PointManagement/MarketplacePoints/index.test.tsx
+++ src/components/PointManagement/MarketplacePoints/index.test.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { ThemeContext } from 'styled-components';
 
+import { defaultTheme } from 'styles/theme';
 import MarketplacePoints from '.';
 
 describe('<MarketplacePoints />', () => {
   test('should be in the document', () => {
     const onChange = jest.fn();
     const { getByText } = render(
-      <MarketplacePoints onChange={onChange} marketplacePoints={200} />,
+      <ThemeContext.Provider value={defaultTheme}>
+        <MarketplacePoints onChange={onChange} marketplacePoints={200} />,
+      </ThemeContext.Provider>,
     );
 
     expect(getByText(/RESGATE NO MARKETPLACE/)).toBeInTheDocument();
