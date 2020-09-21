480196e6b625b065c0a921568a50f5e4b63bed74
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-09-21 17:02:59 -0300

    FMC Team theme

---
+++ src/@types/styled/modules/footer.ts
@@ -0,0 +1,3 @@
+export default interface Footer {
+  background: string;
+}
---
+++ src/styles/theme/modules/footer/fmcTeam.ts
@@ -0,0 +1,4 @@
+export default {
+  background:
+    'linear-gradient(to right, rgb(77, 77, 77) 0%, rgb(117, 116, 116) 100%)',
+};
