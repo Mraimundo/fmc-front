e250cac09a773fd5a80de37a1c31fa58c03dc712
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-10-07 17:01:27 -0300

    Loading on Publish action on Campaign Manager

--- src/pages/CampaignsManagement/List/Main/Table/Actions/Active/style.ts
+++ src/pages/CampaignsManagement/List/Main/Table/Actions/Active/style.ts
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
 export const Container = styled.div`
   display: flex;
-  /*justify-content: center;*/
+  justify-content: center;
+  padding-right: 5px;
 
   svg {
     cursor: pointer;
@@ -13,5 +17,16 @@ export const Container = styled.div`
     &:active {
       transform: scale(1);
     }
+  }
+`;
+
+export const StyledLoader = styled(Loader)`
+  display: flex;
+  align-items: center;
+  margin-right: 5px;
+  color: ${({ theme }) => theme.font.color.primary};
+
+  svg {
+    fill: ${({ theme }) => theme.font.color.primary};
   }
 `;
