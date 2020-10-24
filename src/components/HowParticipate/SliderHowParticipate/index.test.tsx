29bf5abc9d7a41315e082bd5a079b443d1ab17c3
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-10-24 14:33:26 -0300

    Mock Media Screen object to work with Jest

--- src/components/HowParticipate/SliderHowParticipate/index.test.tsx
+++ src/components/HowParticipate/SliderHowParticipate/index.test.tsx
@@ -8,6 +8,20 @@ import { render, getByTestId } from '@testing-library/react';
 import { banners } from 'state/modules/home/mock';
 import Banners from '.';
 
+Object.defineProperty(window, 'matchMedia', {
+  value: () => ({
+    matches: false,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  }),
+});
+
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => ({
+    getPropertyValue: jest.fn(),
+  }),
+});
+
 describe('<Banners />', () => {
   test('should render banners', () => {
     const { container } = render(
