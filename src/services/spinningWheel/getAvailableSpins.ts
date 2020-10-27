143971d9d8af56c8d802098ee6e028a9985ae67a
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-10-26 21:47:55 -0300

    Remove comments

--- src/services/spinningWheel/getAvailableSpins.ts
+++ src/services/spinningWheel/getAvailableSpins.ts
@@ -41,13 +41,4 @@ export default async (quizId: number): Promise<Spin | null> => {
   }
 
   return null;
-
-  /* return [
-    { id: '100 pontos', value: '100 pontos' },
-    { id: 'Tente novamente', value: 'Tente novamente' },
-    { id: '250 pontos', value: '250 pontos' },
-    { id: 'Tenho outra vez', value: 'Tenho outra vez' },
-    { id: '300 pontos', value: '300 pontos' },
-    { id: 'Surpresa', value: 'Surpresa' },
-  ]; */
 };
