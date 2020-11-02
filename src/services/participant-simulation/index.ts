d6f4195938f2a442452cf6559349d52837499646
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-11-02 11:36:07 -0300

    Separate get Participants to Simulate function into a service | Refactor list to work with Filters

--- src/services/participant-simulation/index.ts
+++ src/services/participant-simulation/index.ts
@@ -1,3 +1,4 @@
 import getTokenSimulate from './get-token-simulate';
+import getParticipantsList from './get-participants-list-to-simulate';
 
-export { getTokenSimulate };
+export { getTokenSimulate, getParticipantsList };
