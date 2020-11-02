cf2bcb3fdc3c96a9f92e3c791e0ac76b2f6e8c25
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-11-02 15:39:45 -0300

    Start points simulator structure

---
+++ src/pages/PointsSimulator/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const PointsSimulator: React.FC = () => {
+  return (
+    <Container>
+      <h1>Simulador</h1>
+    </Container>
+  );
+};
+
+export default PointsSimulator;
