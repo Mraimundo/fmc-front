1ec92251d398bdd930a73af1715fd7a3d8de46a6
immarcelo <celloiof@gmail.com>
2020-11-06 14:52:15 -0300

    ajustes cores tema

--- src/styles/theme/modules/font/fmcProdutor.ts
+++ src/styles/theme/modules/font/fmcProdutor.ts
@@ -4,10 +4,8 @@ export default {
   color: {
     primary: '#3B302A',
     secondary: '#000',
-    // secondary: '#B4ACA9',
-    tertiary: '#65554D',
-    // quartenary: '#808285',
-    quartenary: '#3B302A',
+    tertiary: '#fff',
+    quartenary: '#808285',
   },
   fontFamily: { ...FONTS },
 };
