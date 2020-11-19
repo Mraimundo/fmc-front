c61223b3e12a3bf5c674b7bad217bf4f5baae6ac
immarcelo <celloiof@gmail.com>
2020-11-19 18:06:38 -0300

    ajuste no fluxo

--- src/pages/Auth/SignInUp/FormSignUp.tsx
+++ src/pages/Auth/SignInUp/FormSignUp.tsx
@@ -30,6 +30,7 @@ type TypeSelect = 'fmc' | 'participant';
 const FormSignUp: React.FC = () => {
   const [loading, setLoading] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [currentCPF, setCurrentCPF] = useState('');
   const [showFormModal, setShowFormModal] = useState(false);
   const [typeSelected, setTypeSelected] = useState<TypeSelect>('participant');
   const { addToast } = useToast();
@@ -52,21 +53,13 @@ const FormSignUp: React.FC = () => {
         typeSelected === 'participant'
           ? await getParticipantByCpf(param_first_access)
           : await getParticipantByUpn(param_first_access);
-      console.log(participant);
-
       history.push('/firstAccess', participant);
     } catch (e) {
       setLoading(false);
       if (e.response?.data?.message === 'CPF não encontrado') {
-        // setShowModal(true);
         if (validateCpf(param_first_access)) {
-          const participant = {
-            cpf: param_first_access,
-            establishment: { team_receives_points: false },
-            role: { id: 26, identifier: 'produtor', name: 'Produtor' },
-            profile: PROFILES.producer,
-          };
-          history.push('/firstAccess', participant);
+          setShowModal(true);
+          setCurrentCPF(param_first_access);
         } else {
           addToast({
             title: 'CPF inválido',
@@ -84,7 +77,13 @@ const FormSignUp: React.FC = () => {
 
   const handleOpenPreCadastro = () => {
     setShowModal(false);
-    setShowFormModal(true);
+    const participant = {
+      cpf: currentCPF,
+      establishment: { team_receives_points: false },
+      role: { id: 26, identifier: 'produtor', name: 'Produtor' },
+      profile: PROFILES.producer,
+    };
+    history.push('/firstAccess', participant);
   };
 
   const handleSelectType = useCallback(
