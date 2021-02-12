d32b6c50eec09e41cb9ea0b0faf37f24d2002816
Alexandre Novalski <novalski@gmail.com>
2021-02-12 10:06:36 -0300

    change table products header

--- src/components/PointsSimulator/Calculator/Table/Header/index.tsx
+++ src/components/PointsSimulator/Calculator/Table/Header/index.tsx
@@ -20,7 +20,7 @@ const Header: React.FC<Props> = ({ tabSelected }) => {
         POG ({tabSelected === Tab.enhancerProductsTab ? 'Kg/L' : 'US$'})
       </div>
       <div>Estoque</div>
-      <div>Preço Gross (US$)</div>
+      <div>Valor Unitário (US$)</div>
       <div>Faturamento (Kg/L)</div>
       <div>Faturamento (US$)</div>
       <div>POG (Kg/L)</div>
