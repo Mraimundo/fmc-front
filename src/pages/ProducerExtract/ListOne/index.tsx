c444d5458ff71ad33a7814f7f34fbb583686aeda
Mouzinho Raimundo <mouzinho@okn.com.br>
2021-02-17 20:25:44 -0300

    prototigem da SafraProps.

--- src/pages/ProducerExtract/ListOne/index.tsx
+++ src/pages/ProducerExtract/ListOne/index.tsx
@@ -9,15 +9,23 @@ import {
   ListItemContainer,
 } from './styles';
 
-interface Props {
+interface Item {
+  id: number;
+  tipoponto: string;
+  data: string;
+  descricao: string;
+  FMCCOINS: string;
+}
+
+interface SafraProps {
   safra: {
     totalsafra: string;
     safra: string;
-    item: any[];
+    item: Item[];
   };
 }
 
-const ListOne: React.FC<Props> = Props => {
+const ListOne: React.FC<SafraProps> = SafraProps => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
@@ -26,12 +34,12 @@ const ListOne: React.FC<Props> = Props => {
         <Header>
           <div>
             <h3>Safra</h3>
-            <h4>{Props.safra.safra}</h4>
+            <h4>{SafraProps.safra.safra}</h4>
           </div>
           <span></span>
           <div>
             <h3>Total</h3>
-            <h4> {Props.safra.totalsafra} FMC Coins</h4>
+            <h4> {SafraProps.safra.totalsafra} FMC Coins</h4>
           </div>
         </Header>
         {isOpen && (
@@ -41,25 +49,17 @@ const ListOne: React.FC<Props> = Props => {
               <h4>FMC Coins</h4>
             </div>
             <ListItem>
-              {Props.safra.item.map(
-                (item: {
-                  id: number;
-                  tipoponto: string;
-                  data: string;
-                  descricao: string;
-                  status_text: string;
-                  FMCCOINS: string;
-                }) => (
-                  <div key={item.id}>
-                    <div className="list-tipoponto">
-                      <p>{item.tipoponto}</p>
-                      <span>{item.FMCCOINS}</span>
-                    </div>
-                    <div className="list-description">
-                      <p >{item.descricao}</p>
-                    </div>
+              {SafraProps.safra.item.map(item => (
+                <div key={item.id}>
+                  <div className="list-tipoponto">
+                    <p>{item.tipoponto}</p>
+                    <span>{item.FMCCOINS}</span>
                   </div>
-                ),
+                  <div className="list-description">
+                    <p >{item.descricao}</p>
+                  </div>
+                </div>
+              ),
               )}
             </ListItem>
           </ListItemContainer>
