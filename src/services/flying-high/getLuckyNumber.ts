3c291d660131f9b7b1c136ec43bd7dadc9a16a3e
Alexandre Novalski <novalski@gmail.com>
2021-02-17 14:06:08 -0300

    fix mock range lucky number

--- src/services/flying-high/getLuckyNumber.ts
+++ src/services/flying-high/getLuckyNumber.ts
@@ -1,7 +1,7 @@
 export default async (cpf: string): Promise<number> => {
   return new Promise<number>((resolve, reject) => {
     setTimeout(
-      () => resolve(Math.floor(1 + Math.random()*(9999 + 1 - 1))),
+      () => resolve(Math.floor(1 + Math.random()*(99999 + 1 - 1))),
       2000
     );
   });
