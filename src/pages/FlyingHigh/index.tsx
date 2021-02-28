66f319ddb5f0afa5dc923ecfdcb5cc99aee9c0fe
Alexandre Novalski <novalski@gmail.com>
2021-02-28 18:38:33 -0300

    remove theme to fix

--- src/pages/FlyingHigh/index.tsx
+++ src/pages/FlyingHigh/index.tsx
@@ -6,8 +6,8 @@ import HowToWin from 'components/FlyingHigh/HowToWin';
 import RegisterForm from 'components/FlyingHigh/RegisterForm';
 import FlyingHighBanner from 'components/FlyingHigh/Banner';
 import HeaderText from 'components/FlyingHigh/Info/HeaderText';
-import { ThemeContext } from 'styled-components';
-import { producerTheme as fmcProdutorTheme } from 'styles/theme';
+// import { ThemeContext } from 'styled-components';
+// import { producerTheme as fmcProdutorTheme } from 'styles/theme';
 import Footer from 'components/Footer';
 
 import {
@@ -25,27 +25,27 @@ import {
 const FlyingHigh: React.FC = () => {
   return (
     <Container>
-      <ThemeContext.Provider value={fmcProdutorTheme}>
-        <Header>
-          <FmcLogo />
-          <PromoWrapper>
-            <ImageWrapper>
-              <FlyingHighBanner />
-            </ImageWrapper>
-            <FormWrapper>
-              <UpperEllipse />
-              <RegisterForm />
-              <HeaderText />
-            </FormWrapper>
-            <LowerEllipse />
-          </PromoWrapper>
-        </Header>
-        <Info />
-        <HowToWin />
-        <FooterWrapper>
-          <Footer />
-        </FooterWrapper>
-      </ThemeContext.Provider>
+      {/* <ThemeContext.Provider value={fmcProdutorTheme}> */}
+      <Header>
+        <FmcLogo />
+        <PromoWrapper>
+          <ImageWrapper>
+            <FlyingHighBanner />
+          </ImageWrapper>
+          <FormWrapper>
+            <UpperEllipse />
+            <RegisterForm />
+            <HeaderText />
+          </FormWrapper>
+          <LowerEllipse />
+        </PromoWrapper>
+      </Header>
+      <Info />
+      <HowToWin />
+      <FooterWrapper>
+        <Footer />
+      </FooterWrapper>
+      {/* </ThemeContext.Provider> */}
       <Contact initialPosition="right-bottom" />
     </Container>
   );
