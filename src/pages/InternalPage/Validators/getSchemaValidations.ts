e1fa4e28695a2063f9b19184712e06078d894934
MouRaimundo <mouzinho@okn.com.br>
2021-04-29 19:01:18 -0300

    ultima branch de pesquisas

--- src/pages/InternalPage/Validators/getSchemaValidations.ts
+++ src/pages/InternalPage/Validators/getSchemaValidations.ts
@@ -3,5 +3,7 @@ import * as Yup from 'yup';
 const mandatoryMessage = 'Campo obrigatório';
 
 const schemaValidation = Yup.object().shape({
-
+  name: Yup.string().required(mandatoryMessage),
+  time: Yup.string().required(mandatoryMessage),
+  date: Yup.string().required(mandatoryMessage),
 });
